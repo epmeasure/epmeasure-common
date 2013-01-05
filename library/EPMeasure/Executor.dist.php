051166ef1b39e23f61b99b7b1228b19446500da4
ingo <ingo.walz@googlemail.com>
2013-01-05 07:12:46 +0100

    Add runtime measurement

--- library/EPMeasure/Executor.dist.php
+++ library/EPMeasure/Executor.dist.php
@@ -2,8 +2,16 @@
 require_once __DIR__.'/../library/EPMeasure/SerializeableClosure.php';
 $exec = unserialize('{exec}');
 ob_start();
+$start = microtime(true);
 $exec();
+$stop = microtime(true);
+$runtime = $stop - $start;
 ob_end_clean();
 
+// To fast to measure
+if (strpos((string)$runtime, "E") !== false) {
+    $runtime = 0.0000;
+}
+
 echo "Memory:" . memory_get_peak_usage(true) . PHP_EOL;
-echo "Runtime:" . PHP_EOL;
+echo "Runtime:" . (float)$runtime . PHP_EOL;
